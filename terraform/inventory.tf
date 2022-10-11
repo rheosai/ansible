ea0b74d1e263b5a78ddc507b3291a002c4a99d06
Murthy Karra <karrad@rheos.ai>
2022-10-11 16:37:40 -0700

    Updated Inventory File

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -1,13 +1,13 @@
 resource "local_file" "ansible" {
     content  = <<-EOT
     [nodes]
-    ubuntu1 ansible_ssh_host=${aws_instance.ubuntu1.public_ip}
+    ubuntu1 ansible_ssh_host=${aws_instance.ubuntu1.public_ip} ansible_user=ubuntu
     redhat1 ansible_ssh_host=${aws_instance.redhat1.public_ip}
     suse1 ansible_ssh_host=${aws_instance.suse1.public_ip}
     amazon1 ansible_ssh_host=${aws_instance.amazon1.public_ip}
 
     [all:vars]
-    ansible_user=ubuntu
+    ansible_user=ec2-user
     ansible_become=yes
     ansible_become_method=sudo
     host_key_checking=False
