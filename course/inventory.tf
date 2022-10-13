30d66440d23e27e8143981309793cdfb20766bf2
Murthy Karra <karrad@rheos.ai>
2022-10-12 19:05:11 -0700

    New Directory

--- course/inventory.tf
+++ course/inventory.tf
@@ -1,17 +1,11 @@
 resource "local_file" "ansible" {
     content  = <<-EOT
     [nodes]
-    ubuntu1 ansible_ssh_host=${aws_instance.ubuntu1.public_ip} ansible_user=ubuntu
+    ubuntu1 ansible_ssh_host=${aws_instance.ubuntu1.public_ip}
     redhat1 ansible_ssh_host=${aws_instance.redhat1.public_ip}
     suse1 ansible_ssh_host=${aws_instance.suse1.public_ip}
     amazon1 ansible_ssh_host=${aws_instance.amazon1.public_ip}
 
-    [all:vars]
-    ansible_user=ec2-user
-    ansible_become=yes
-    ansible_become_method=sudo
-    host_key_checking=False
-    ansible_ssh_private_key_file=~/.ssh/id_rsa
     EOT
     filename = "${path.module}/inventory"
 }
