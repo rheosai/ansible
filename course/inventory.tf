5d8a25d97bd0e51f60d0d6b88d3f2e84a1429866
Murthy Karra <karrad@rheos.ai>
2022-10-13 17:57:38 -0700

    Bare inventory in AWS

--- course/inventory.tf
+++ course/inventory.tf
@@ -1,10 +1,10 @@
 resource "local_file" "ansible" {
     content  = <<-EOT
-    [nodes]
-    ubuntu1 ansible_ssh_host=${aws_instance.ubuntu1.public_ip}
-    redhat1 ansible_ssh_host=${aws_instance.redhat1.public_ip}
-    suse1 ansible_ssh_host=${aws_instance.suse1.public_ip}
-    amazon1 ansible_ssh_host=${aws_instance.amazon1.public_ip}
+    
+    ${aws_instance.ubuntu1.public_ip}
+    ${aws_instance.redhat1.public_ip}
+    ${aws_instance.suse1.public_ip}
+    ${aws_instance.amazon1.public_ip}
 
     EOT
     filename = "${path.module}/inventory"
