d53efab27f77638c830a86ad53a60e8562611176
Murthy Karra <karrad@rheos.ai>
2022-10-13 21:31:08 -0700

    Re-arranged inventory

--- course/inventory.tf
+++ course/inventory.tf
@@ -2,8 +2,8 @@ resource "local_file" "ansible" {
     content  = <<-EOT
     
     ${aws_instance.ubuntu1.public_ip}
-    ${aws_instance.redhat1.public_ip}
     ${aws_instance.suse1.public_ip}
+    ${aws_instance.redhat1.public_ip}
     ${aws_instance.amazon1.public_ip}
 
     EOT
